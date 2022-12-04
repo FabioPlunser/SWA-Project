2520948c7d63a4ed3ac20a4c4712102a90d76a34
davirieser <david.w.rieser@gmail.com>
2022-12-04 17:34:48 +0100

    Added Dummy Test

--- swa/src/test/java/at/ac/uibk/swa/SwaApplicationTests.java
+++ swa/src/test/java/at/ac/uibk/swa/SwaApplicationTests.java
@@ -1,9 +1,15 @@
 package at.ac.uibk.swa;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SwaApplicationTests {
+
+    @Test
+    public void DummyTest() {
+        Assertions.assertEquals(2 + 2, 4);
+    }
 
 }
