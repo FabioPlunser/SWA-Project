f9c518202bc4b8fc00579a2a0de27f7c0fa5df13
Fabio Plunser <f.plunser@outlook.com>
2022-12-04 14:09:50 +0100

    Added Page for learning Cards

---
+++ static/src/learn/Learn.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import FlipCard from "../lib/components/flipCard.svelte";
+    import Slider from "../lib/components/slider.svelte";
+	import Nav from "../lib/components/nav.svelte";
+</script>
+
+<svelte:head>
+    <title>Learn View</title>
+    <link rel="icon" type="image/gif" href="https://media.giphy.com/avatars/KirstenHurley/kdEReo8fnjUs/200h.gif"/>
+</svelte:head>
+
+
+<Nav title="Learnview"/>
+<main class="mt-20">
+	<div class="grid grid-row gap-6 justify-center">
+		<FlipCard />
+		<div class="flex">
+			<Slider/>
+			<button class="btn btn-primary">Next</button>
+		</div>
+	</div>
+</main>
+
---
+++ static/src/learn/index.ts
@@ -0,0 +1,11 @@
+import "/app.postcss";
+import App from "./Learn.svelte";
+
+// import '@brainandbones/skeleton/themes/theme-skeleton.css';
+
+const app = new App({
+  target: document.getElementById("app"),
+});
+
+export default app;
+
