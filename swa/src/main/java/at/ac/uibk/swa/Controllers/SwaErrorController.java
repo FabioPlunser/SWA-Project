f059cd1ca3ddbd87a204af9d5313e0f4ab8865e6
davirieser <david.w.rieser@gmail.com>
2022-12-04 13:33:00 +0100

    Changed Error Page to return JSON

--- swa/src/main/java/at/ac/uibk/swa/Controllers/SwaErrorController.java
+++ swa/src/main/java/at/ac/uibk/swa/Controllers/SwaErrorController.java
@@ -1,5 +1,7 @@
 package at.ac.uibk.swa.Controllers;
 
+import at.ac.uibk.swa.Models.RestResponses.MessageResponse;
+import at.ac.uibk.swa.Models.RestResponses.RestResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
@@ -10,11 +12,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SwaErrorController implements ErrorController {
     @RequestMapping("/error")
     @ResponseBody
-    public String handleError(HttpServletRequest request) {
+    public RestResponse handleError(HttpServletRequest request) {
+        // TODO: This is hardly human readable if they get redirected to a invalid Endpoint
+        //       => There has to be a way to check which stage failed (Auth or Endpoint or Exception)
+        return new MessageResponse(false, "Incorrect or invalid Token");
+        /*
         Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
         Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
         return String.format("<html><body><h2>Error Page</h2><div>Status code: <b>%s</b></div>"
                         + "<div>Exception Message: <b>%s</b></div><body></html>",
                 statusCode, exception==null? "N/A": exception.getMessage());
+         */
     }
 }
