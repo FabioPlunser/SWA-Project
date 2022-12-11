284c5d901b36d2e81fbb58659bb30f75882faef8
Fabio Plunser <f.plunser@outlook.com>
2022-12-11 20:43:15 +0100

    updated navbar to be more user friendly

--- static/src/lib/components/nav.svelte
+++ static/src/lib/components/nav.svelte
@@ -1,30 +1,23 @@
 <script lang="ts">
-  export let title = "";
   import favicon from "/favicon.png";
-  import { redirect } from "../utils/redirect";
-  import { token } from "../stores/token";
-  function handleLogout(){
-      $token = null;
-      redirect("login");
-  }
+  export let title = "";
+  export let buttons = [];
 </script>
 
 <div class="flex p-2 justify-center bg-slate-900 w-full rounded-none justiy-center shadow-2xl top-0 fixed z-10">
     <div class="flex-1 flex">
       <a href="/" class=""><img src={favicon} alt="logo" class="w-12 rounded-full shadow-xl"/></a>
-      <div class="flex justify-center mx-auto items-center">
-          <h1 class="text-2xl underline">{title}</h1>
-      </div>
-     
+      <h1 class="flex ml-2 text-2xl underline items-center">{title}</h1>
     </div>
-    <div class="text-xl">
-        
-        <ul class="menu menu-horizontal p-0 mx-auto justify-center">
-            
-        <li>
-          <button class="rounded-xl" on:click={handleLogout}>
-            Logout
-          </button>
-      </ul>
+
+    <div class="menu menu-horizontal">
+      {#each buttons as button}
+      {#if button.tag === "label"}
+        <label for={button.id} class="flex justify-center btn btn-primary mx-2" on:click={button.action}>{button.text}</label>
+      {/if}
+      {#if button.tag === "button"}
+        <button id={button.id} class="flex justify-center btn btn-primary mx-2" on:click={button.action}>{button.text}</button>
+      {/if}
+      {/each}
     </div>
-  </div>
+</div>
