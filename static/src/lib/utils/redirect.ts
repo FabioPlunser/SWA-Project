5c0a84bd4a75dbb551626c814bd8790a463a5686
Fabio Plunser <f.plunser@outlook.com>
2022-12-14 12:00:02 +0100

    changed redirect to really redirect instead of replace url

--- static/src/lib/utils/redirect.ts
+++ static/src/lib/utils/redirect.ts
@@ -1,3 +1,3 @@
 export function redirect (pageName: string){
-    window.location.replace(window.location.origin + `/${pageName}`);
+    window.location.href = window.location.origin + `/${pageName}`;
 }
