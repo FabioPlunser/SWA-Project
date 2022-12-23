7fecf162b0b3c2349173226413eb9abdd6f97421
Fabian Margreiter <fabian.margreiter@gmail.com>
2022-12-23 10:48:56 +0100

    Added functionality to StringGenerator for tests

--- swa/src/test/java/at/ac/uibk/swa/DeckServiceTest.java
+++ swa/src/test/java/at/ac/uibk/swa/DeckServiceTest.java
@@ -28,20 +28,6 @@ public class DeckServiceTest {
     private PersonService personService;
 
     @Test
-    public void testCreateDeck() {
-        // given: person and deck created by person
-        Person person = new Person("person-testCreatingDeck", "", "", Set.of());
-        Deck deck = new Deck("deck-testCreatingDeck", "", person);
-        assertTrue(personService.save(person), "Unable to save user for test");
-        assertTrue(deckService.save(deck), "Unable to save deck for test");
-
-        // when: loading person and created decks
-        Optional<Person> maybePerson = personService.login(person.getUsername(), person.getPasswdHash());
-        assertTrue(maybePerson.isPresent(), "Unable to login user");
-        List<Deck> createdDecks = maybePerson.get().getCreatedDecks();
-
-        // then: user should have exactly one created deck and deck id should be as defined in beginning
-        assertEquals(1, createdDecks.size(), "Unexpectedly received " + createdDecks.size() + " decks");
-        assertEquals(deck.getDeckId(), createdDecks.get(0).getDeckId(), "Got deck " + createdDecks.get(0).getDeckId() + " when deck " + deck.getDeckId() + " was expected");
+    public void testSaveDecks() {
     }
 }
