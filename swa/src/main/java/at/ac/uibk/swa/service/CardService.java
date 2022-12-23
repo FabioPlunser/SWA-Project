f07b80697e89052ec4080f361182f2bd01f383e6
Fabian Margreiter <fabian.margreiter@gmail.com>
2022-12-23 15:49:37 +0100

    Created stub for method getAllCards() to get all cards for specific deck

--- swa/src/main/java/at/ac/uibk/swa/service/CardService.java
+++ swa/src/main/java/at/ac/uibk/swa/service/CardService.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service("cardService")
 public class CardService {
-
     @Autowired
     CardRepository cardRepository;
+    @Autowired
+    DeckService deckService;
 
     /**
      * Gets all existing cards from the repository
@@ -29,5 +31,15 @@ public class CardService {
         } catch (Exception e) {
             return false;
         }
+    }
+
+    /**
+     * Gets all existing cards for a specific deck from the repository
+     *
+     * @param deckId id of the deck for which the cards should be retrieved
+     * @return a list of all the cards in the deck
+     */
+    public List<Card> getAllCards(UUID deckId) {
+        return null;
     }
 }
