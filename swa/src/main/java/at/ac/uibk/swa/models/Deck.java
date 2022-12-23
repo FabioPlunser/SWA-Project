a87e71d9b6cc0eb80b916f694a5a9b0e74ed66ff
Fabian Margreiter <fabian.margreiter@gmail.com>
2022-12-23 11:11:46 +0100

    Created unittest for saving and loading alls decks (not yet functional, as service is missing functionality

--- swa/src/main/java/at/ac/uibk/swa/models/Deck.java
+++ swa/src/main/java/at/ac/uibk/swa/models/Deck.java
@@ -76,6 +76,7 @@ public class Deck implements Serializable {
     @ManyToMany(mappedBy = "savedDecks", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Person> subscribedPersons = new ArrayList<>();
 
+    //TODO: Change name - get subscribes, etc.
     public Person[] getPersons() {
         return Stream.concat(
                     this.subscribedPersons.stream(),
--- swa/src/test/java/at/ac/uibk/swa/DeckServiceTest.java
+++ swa/src/test/java/at/ac/uibk/swa/DeckServiceTest.java
@@ -5,17 +5,15 @@ import at.ac.uibk.swa.models.Person;
 import at.ac.uibk.swa.service.CardService;
 import at.ac.uibk.swa.service.DeckService;
 import at.ac.uibk.swa.service.PersonService;
+import at.ac.uibk.swa.util.StringGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -28,6 +26,50 @@ public class DeckServiceTest {
     private PersonService personService;
 
     @Test
-    public void testSaveDecks() {
+    public void testSaveAndGetDecks() {
+        // given: demo creators and decks saved to database
+        int numberOfCreators = 5;
+        int numberOfDecksPerCreator = 10;
+        List<Person> creators = new ArrayList<>();
+        Map<Person, Deck> savedDecks = new HashMap<>();
+        for (int i = 0; i < numberOfCreators; i++) {
+            Person creator = new Person(
+                    "person-TestSaveAndGetDecks-" + (i+1),
+                    StringGenerator.email(),
+                    StringGenerator.password(),
+                    Set.of()
+            );
+            assertTrue(personService.save(creator), "Could not save user");
+            creators.add(creator);
+            for (int j = 0; j < numberOfDecksPerCreator; j++) {
+                Deck deck = new Deck(
+                        "deck-TestSaveAndGetDecks-" + (j+1),
+                        StringGenerator.deckDescription(),
+                        creator
+                );
+                assertTrue(deckService.save(deck), "Could not save deck");
+                savedDecks.put(creator, deck);
+            }
+        }
+
+        // TODO: Implement functionality on deckService
+
+        // when: loading all decks from database
+        List<Deck> loadedDecks = new ArrayList<>();//deckService.getAllDecks();
+
+        // then: all saved decks must be found again and attributes must be identical
+        for (Map.Entry<Person,Deck> entry : savedDecks.entrySet()) {
+            Person creator = entry.getKey();
+            Deck deck = entry.getValue();
+            assertTrue(loadedDecks.contains(deck), "Could not find deck " + deck);
+            Deck loadedDeck = loadedDecks.get(loadedDecks.indexOf(deck));
+            assertEquals(creator, loadedDeck.getCreator(), "Wrong creator of " + deck);
+            assertEquals(deck.getName(), loadedDeck.getName(), "Wrong name of " + deck);
+            assertEquals(deck.getDescription(), loadedDeck.getDescription(), "Wrong description of " + deck);
+            assertFalse(loadedDeck.isBlocked(), deck + " has been blocked");
+            assertFalse(loadedDeck.isDeleted(), deck + " has been deleted");
+            assertFalse(loadedDeck.isPublished(), deck + " has been published");
+        }
+
     }
 }
