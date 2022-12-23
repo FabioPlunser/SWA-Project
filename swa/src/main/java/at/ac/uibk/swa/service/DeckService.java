bdc31aa6f95161cbed66f0a34d740614d69066d9
Fabian Margreiter <fabian.margreiter@gmail.com>
2022-12-23 11:35:47 +0100

    Implemented getAllDecks() method

--- swa/src/main/java/at/ac/uibk/swa/service/DeckService.java
+++ swa/src/main/java/at/ac/uibk/swa/service/DeckService.java
@@ -1,15 +1,28 @@
 package at.ac.uibk.swa.service;
 
 import at.ac.uibk.swa.models.Deck;
+import at.ac.uibk.swa.models.Person;
 import at.ac.uibk.swa.repositories.DeckRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.function.Predicate;
 
 @Service("deckService")
 public class DeckService {
 
     @Autowired
     DeckRepository deckRepository;
+
+    /**
+     * Gets all decks from the repository no matter if deleted, blocked, published, etc.
+     *
+     * @return  a list of all decks
+     */
+    public List<Deck> getAllDecks() {
+        return deckRepository.findAll();
+    }
 
     public boolean save(Deck deck) {
         try {
