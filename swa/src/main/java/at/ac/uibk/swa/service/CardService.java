f8be9be373669394a026f0849179486ef6d08c02
Fabian Margreiter <fabian.margreiter@gmail.com>
2022-12-23 15:53:26 +0100

    Implemented method getAllCards() to get all cards for specific deck

--- swa/src/main/java/at/ac/uibk/swa/service/CardService.java
+++ swa/src/main/java/at/ac/uibk/swa/service/CardService.java
@@ -1,11 +1,14 @@
 package at.ac.uibk.swa.service;
 
 import at.ac.uibk.swa.models.Card;
+import at.ac.uibk.swa.models.Deck;
 import at.ac.uibk.swa.repositories.CardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service("cardService")
@@ -35,11 +38,15 @@ public class CardService {
 
     /**
      * Gets all existing cards for a specific deck from the repository
+     * NOTE: if deck is not found (wrong id) no cards will be returned
      *
      * @param deckId id of the deck for which the cards should be retrieved
      * @return a list of all the cards in the deck
      */
     public List<Card> getAllCards(UUID deckId) {
-        return null;
+        List<Card> cards = new ArrayList<>();
+        Optional<Deck> maybeDeck = deckService.findById(deckId);
+        if (maybeDeck.isPresent()) cards = maybeDeck.get().getCards();
+        return cards;
     }
 }
