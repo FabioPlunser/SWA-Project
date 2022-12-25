67e66971144c3c7894151d3b1d92a0f5d2e89900
Fabian Margreiter <fabian.margreiter@gmail.com>
2022-12-25 14:35:54 +0100

    Minor refactoring in UserDeckServiceTests

--- swa/src/test/java/at/ac/uibk/swa/UserDeckServiceTest.java
+++ swa/src/test/java/at/ac/uibk/swa/UserDeckServiceTest.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import javax.print.DocFlavor;
 import java.util.*;
 import java.util.function.Predicate;
 
