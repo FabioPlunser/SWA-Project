11a1d1493b1702e4a94c31597296d8b5baefa60a
Fabian Margreiter <fabian.margreiter@gmail.com>
2022-12-25 19:30:26 +0100

    Implemented unittests to get cards from subscribed decks (published and unpublished)

--- swa/src/test/java/at/ac/uibk/swa/service/card_service/CardServiceTestGetFromDeck.java
+++ swa/src/test/java/at/ac/uibk/swa/service/card_service/CardServiceTestGetFromDeck.java
@@ -94,4 +94,73 @@ public class CardServiceTestGetFromDeck {
         // then: no cards must be loaded
         assertEquals(0, loadedCards.size(), "Got wrong number of cards");
     }
+
+    @Test
+    public void testGetCardsFromDeckOwnedDeleted() {
+        // given: a user, that created a deck with multiple cards, where the deck got deleted after creation
+        int numCardsPerDeck = 10;
+        Person person = createUser("person-testGetCardsFromDeckOwnedDeleted");
+        Deck deck = createDeck("deck-testGetCardsFromDeckOwnedDeleted", person);
+        List<Card> cards = new ArrayList<>();
+        for (int i = 0; i < numCardsPerDeck; i++) {
+            Card card = new Card(StringGenerator.cardText(), StringGenerator.cardText(),false, deck);
+            assertTrue(cardService.create(card), "Unable to create card");
+            cards.add(card);
+        }
+        assertTrue(userDeckService.delete(deck), "Unable to delete deck");
+
+        // when: retrieving all cards for that user and deck
+        List<Card> loadedCards = cardService.getAllCards(deck, person);
+
+        // then: no cards must be loaded
+        assertEquals(0, loadedCards.size(), "Got wrong number of cards");
+    }
+
+    @Test
+    public void testGetCardsFromDeckSubscribedPublished() {
+        // given: a user, that subscribed to a deck with multiple cards
+        int numCardsPerDeck = 10;
+        Person person = createUser("person-testGetCardsFromDeckSubscribedPublished");
+        Deck deck = createDeck("deck-testGetCardsFromDeckSubscribedPublished", createUser("person-testGetCardsFromDeckSubscribedPublished-creator"));
+        List<Card> cards = new ArrayList<>();
+        for (int i = 0; i < numCardsPerDeck; i++) {
+            Card card = new Card(StringGenerator.cardText(), StringGenerator.cardText(),false, deck);
+            assertTrue(cardService.create(card), "Unable to create card");
+            cards.add(card);
+        }
+        assertTrue(userDeckService.publish(deck), "Unable to publish deck");
+        assertTrue(userDeckService.subscribe(deck, person), "Unable to subscribe to deck");
+
+        // when: retrieving all cards for that user and deck
+        List<Card> loadedCards = cardService.getAllCards(deck, person);
+
+        // then: all cards must be loaded again
+        assertEquals(cards.size(), loadedCards.size(), "Got wrong number of cards");
+        for (Card card : cards) {
+            assertTrue(loadedCards.contains(card), "Unable to find a card");
+        }
+    }
+
+    @Test
+    public void testGetCardsFromDeckSubscribedUnpublished() {
+        // given: a user, that subscribed to a deck with multiple cards, but the deck was unpublished after subscription
+        int numCardsPerDeck = 10;
+        Person person = createUser("person-testGetCardsFromDeckSubscribedUnpublished");
+        Deck deck = createDeck("deck-testGetCardsFromDeckSubscribedUnpublished", createUser("person-testGetCardsFromDeckSubscribedUnpublished-creator"));
+        List<Card> cards = new ArrayList<>();
+        for (int i = 0; i < numCardsPerDeck; i++) {
+            Card card = new Card(StringGenerator.cardText(), StringGenerator.cardText(),false, deck);
+            assertTrue(cardService.create(card), "Unable to create card");
+            cards.add(card);
+        }
+        assertTrue(userDeckService.publish(deck), "Unable to publish deck");
+        assertTrue(userDeckService.subscribe(deck, person), "Unable to subscribe to deck");
+        assertTrue(userDeckService.unpublish(deck), "Unable to unpublish deck");
+
+        // when: retrieving all cards for that user and deck
+        List<Card> loadedCards = cardService.getAllCards(deck, person);
+
+        // then: no cards must be loaded
+        assertEquals(0, loadedCards.size(), "Got wrong number of cards");
+    }
 }
