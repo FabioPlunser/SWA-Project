e0ee8dc2606dc1a0c12f88743d7c2e85cf5434b1
davirieser <david.w.rieser@gmail.com>
2022-12-28 20:26:59 +0100

    Fixed minor errors in Learning Algorithm

--- swa/src/main/java/at/ac/uibk/swa/service/card_service/learning_algorithm/LearningAlgorithm.java
+++ swa/src/main/java/at/ac/uibk/swa/service/card_service/learning_algorithm/LearningAlgorithm.java
@@ -2,36 +2,47 @@ package at.ac.uibk.swa.service.card_service.learning_algorithm;
 
 import at.ac.uibk.swa.models.LearningProgress;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 
 public class LearningAlgorithm {
     public static LearningProgress getUpdatedLearningProgress(
             LearningProgress current,
             int difficulty
     ) {
-        LearningProgress newLearningProgress = new LearningProgress();
-        newLearningProgress.setRepetitions(current.getRepetitions() + 1);
+        // Copy the old LearningProgress
+        LearningProgress newLearningProgress = current.copy();
 
+        // Increment the number of times this card was learned
+        newLearningProgress.incrementRepetitions();
+
+        // Fail: Reset the Interval
         if (difficulty < 3) {
             newLearningProgress.setInterval(1);
         }
-        if (difficulty < 4) {
-            //TODO warum nicht LocalDate statt Date?
-            newLearningProgress.setNextLearn(Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant()));
-        }
+
+        // Answer was right => Adjust interval and eFactor.
         if (difficulty > 2) {
-            if (current.getRepetitions() == 1) {
+            int n = newLearningProgress.getRepetitions();
+            if (n == 1) {
                 newLearningProgress.setInterval(1);
-            } else if (current.getRepetitions() == 2) {
+            } else if (n == 2) {
                 newLearningProgress.setInterval(6);
             } else {
                 newLearningProgress.setInterval((int) (current.getInterval() * current.getEFactor()));
-                newLearningProgress.setEFactor(Math.max(1.3, current.getEFactor() - 0.8 + 0.28*difficulty - 0.02*difficulty*difficulty));
+                newLearningProgress.setEFactor(Math.max(1.3, current.getEFactor() - 0.8 + 0.28 * difficulty - 0.02 * difficulty * difficulty));
             }
         }
+
+        // Card was not learned enough => Enqueue Card again.
+        if (difficulty < 4) {
+            // Adding the Card to the Queue again is implemented on the Front-End.
+            // But if the user does not learn the Card, ensure that the card's nextLearn Date is not changed.
+            return newLearningProgress;
+        }
+
+        // Calculate the time when the card should be learned the next time.
+        LocalDateTime nextLearn = LocalDateTime.now().plusDays(newLearningProgress.getInterval());
+        newLearningProgress.setNextLearn(nextLearn);
 
         return newLearningProgress;
     }
