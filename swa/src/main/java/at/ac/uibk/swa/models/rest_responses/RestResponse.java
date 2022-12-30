42cd3fa0e053942e6aea3b11db82b604e9734203
davirieser <david.w.rieser@gmail.com>
2022-12-30 19:52:48 +0100

    Added non-JSON "statusCode"-Field to RestResponse

--- swa/src/main/java/at/ac/uibk/swa/models/rest_responses/RestResponse.java
+++ swa/src/main/java/at/ac/uibk/swa/models/rest_responses/RestResponse.java
@@ -1,8 +1,11 @@
 package at.ac.uibk.swa.models.rest_responses;
 
+import at.ac.uibk.swa.util.SerializationUtil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 import java.io.Serializable;
 
@@ -15,18 +18,38 @@ import java.io.Serializable;
  * @version 1.0
  */
 @Getter
-@NoArgsConstructor(access = AccessLevel.MODULE)
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.MODULE)
 public class RestResponse implements Serializable {
+
+    public RestResponse(boolean success) {
+        this(success, success ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+    }
+
+    public RestResponse(boolean success, HttpStatus status) {
+        this(success, status.value());
+    }
+
+    public RestResponse(boolean success, HttpStatusCode httpStatusCode) {
+        this(success, httpStatusCode.value());
+    }
 
     /**
      * Indicates the Success-State of an Operation to the Front-End.
      */
     @Setter(AccessLevel.PROTECTED)
-    private boolean success = false;
+    private boolean success;
+
+    @JsonIgnore
+    @Setter(AccessLevel.PROTECTED)
+    private int statusCode;
+
+    public int getStatusCode() {
+        return this.statusCode;
+    }
 
     @SuppressWarnings("unused")
     public String toResponse() throws JsonProcessingException {
-        return new ObjectMapper().writeValueAsString(this);
+        return SerializationUtil.serializeJSON(this);
     }
 }
