4203b83709b5ccfd450071cc6bc030c679aabc91
davirieser <david.w.rieser@gmail.com>
2023-01-01 04:53:48 +0100

    Implemented GrantedAuthority for Permission

--- swa/src/main/java/at/ac/uibk/swa/models/Permission.java
+++ swa/src/main/java/at/ac/uibk/swa/models/Permission.java
@@ -1,13 +1,24 @@
 package at.ac.uibk.swa.models;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import java.util.Set;
 
-public enum Permission {
+public enum Permission implements GrantedAuthority {
     USER,
     ADMIN;
 
     public static Set<Permission> defaultPermissions() {
         return Set.of(USER);
+    }
+
+    public static Set<Permission> defaultAuthorities() {
+        return Set.of(USER);
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.toString();
     }
 }
 
