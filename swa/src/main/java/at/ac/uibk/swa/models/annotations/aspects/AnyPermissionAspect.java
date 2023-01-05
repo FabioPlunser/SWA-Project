099dad0bcf595f7e45ef8f8afb44649420ce9883
davirieser <david.w.rieser@gmail.com>
2023-01-05 22:46:11 +0100

    Changed Permission Annotations to use RequestContext

--- swa/src/main/java/at/ac/uibk/swa/models/annotations/aspects/AnyPermissionAspect.java
+++ swa/src/main/java/at/ac/uibk/swa/models/annotations/aspects/AnyPermissionAspect.java
@@ -4,11 +4,14 @@ import at.ac.uibk.swa.config.person_authentication.AuthContext;
 import at.ac.uibk.swa.models.Authenticable;
 import at.ac.uibk.swa.models.Permission;
 import at.ac.uibk.swa.models.annotations.AnyPermission;
+import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +23,9 @@ import java.util.stream.Collectors;
 @Aspect
 @Component
 public class AnyPermissionAspect {
+    @Autowired
+    private HttpServletRequest request;
+
     @Around("@annotation(at.ac.uibk.swa.models.annotations.AnyPermission)")
     public Object doSomething(ProceedingJoinPoint jp) throws Throwable {
         // Get the Permissions that are needed from the Attribute
@@ -31,7 +37,10 @@ public class AnyPermissionAspect {
                 ).collect(Collectors.toSet());
 
         // Try to get the currently logged-in user
-        Optional<Set<GrantedAuthority>> maybeUserPermissions = AuthContext.getCurrentUser().map(Authenticable::getPermissions);
+        Optional<Set<GrantedAuthority>> maybeUserPermissions =
+                Optional.ofNullable((UsernamePasswordAuthenticationToken) request.getUserPrincipal())
+                .map(token -> token.getPrincipal() instanceof Authenticable a ? a : null)
+                .map(Authenticable::getPermissions);
 
         // If no user is logged in => No Permissions => Fail
         if (maybeUserPermissions.isPresent()) {
