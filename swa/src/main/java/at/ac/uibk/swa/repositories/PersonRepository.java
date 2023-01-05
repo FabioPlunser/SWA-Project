f74ed648a22ab969e983d11b7f88a4aeae5034b2
davirieser <david.w.rieser@gmail.com>
2023-01-05 18:25:29 +0100

    Changed login and logout to only change Token

--- swa/src/main/java/at/ac/uibk/swa/repositories/PersonRepository.java
+++ swa/src/main/java/at/ac/uibk/swa/repositories/PersonRepository.java
@@ -1,7 +1,11 @@
 package at.ac.uibk.swa.repositories;
 
 import at.ac.uibk.swa.models.Person;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +18,9 @@ public interface PersonRepository extends CrudRepository<Person, UUID> {
     Optional<Person> findByToken(UUID token);
 
     Optional<Person> findByUsername(String username);
+
+    @Transactional
+    @Modifying
+    @Query("update Person p set p.token = :token where p.id = :id")
+    int updateToken(@Param("id") UUID id, @Param("token") UUID token);
 }
