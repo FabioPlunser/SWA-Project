ee4dee1d9195c23a1833e8bb661ec4a902587480
Fabio Plunser <f.plunser@outlook.com>
2023-01-08 21:23:34 +0100

    added logo and name to login, fixed login button once login failed

--- static/src/lib/components/nav.svelte
+++ static/src/lib/components/nav.svelte
@@ -7,7 +7,7 @@
 <div class="flex p-2 justify-center bg-slate-900 w-full rounded-none justiy-center shadow-2xl top-0 fixed z-10">
     <div class="flex-1 flex">
       <a href="/" class=""><img src={favicon} alt="logo" class="w-12 rounded-full shadow-xl"/></a>
-      <h1 class="flex ml-2 text-2xl underline items-center">{title}</h1>
+      <h1 class="flex ml-2 text-3xl font-bold items-center">{title}</h1>
     </div>
 
     <div class="menu menu-horizontal">
