4d024b917f5ed56c69193826fa7710086b6d1678
Fabio Plunser <f.plunser@outlook.com>
2023-01-09 11:23:24 +0100

    added bootstrap icons

--- static/src/App.svelte
+++ static/src/App.svelte
@@ -288,10 +288,8 @@
 		<div class="flex justify-center">
 			<div class="flex justify-center mx-auto w-full">
 				<label class="input-group flex justify-center w-full ">
-				<span class=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-					<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-				  </svg></span>
-				<input type="text" placeholder="Search for Deck" class="input input-bordered bg-slate-900" bind:value={searchDeck}/>
+					<span><i class="bi bi-search"/></span>
+					<input type="text" placeholder="Search for Deck" class="input input-bordered bg-slate-900" bind:value={searchDeck}/>
 				</label>
 			</div>
 		</div>
@@ -392,9 +390,7 @@
 			
 			<div class="flex justify-center mx-auto w-full">
 				<label class="input-group flex justify-center w-full ">
-				<span class=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-					<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-				  </svg></span>
+				<span><i class="bi bi-search"/></span>
 				<input type="text" placeholder="Search for Deck" class="input input-bordered bg-slate-900" bind:value={adminSearch}/>
 				</label>
 			</div>
