bd123ee6589c35b1258976f0438b86aa2a25373e
Fabio Plunser <f.plunser@outlook.com>
2023-01-14 15:36:34 +0100

    fixed list cards ui

--- static/src/index/listCardsModal.svelte
+++ static/src/index/listCardsModal.svelte
@@ -15,23 +15,25 @@
 
 
 <Modal open={listCards} on:close={()=>listCards=false} closeOnBodyClick={true}>
-    <div class="relative max-w-full">
-        <div class="flex flex-col min-w-fit">
-            <h1 class="flex justify-center text-2xl font-bold">Cards of Deck {selectedDeck.name}</h1>
-            <div class="absolute right-0">
-                <button class="btn btn-primary" on:click={()=> listCards = false}>Close</button>
-            </div>
-            <div class="grid grid-cols-4 gap-2 mt-6">
-                {#await getCardsFromDeck(selectedDeck)}
-                    <Spinner/>
-                {:then cards}
-                    {#each cards as card, index (card.cardId)}
-                        <div>
-                            <DualSideCard {card} {index} editable={false} cardBg="bg-slate-800" textBg="bg-slate-700"/>
-                        </div>
-                    {/each}
-                {/await}
-            </div>
+    <div class="max-w-full min-w-fit">
+        <h1 class="flex justify-center text-2xl font-bold">Cards of Deck {selectedDeck.name}</h1>
+        <br class="mt-4"/>
+        <div class="grid grid-cols-4 gap-2">
+            {#await getCardsFromDeck(selectedDeck)}
+                <Spinner/>
+            {:then cards}
+                {#each cards as card, index (card.cardId)}
+                    <div>
+                        <DualSideCard {card} {index} editable={false} cardBg="bg-slate-800" textBg="bg-slate-700"/>
+                    </div>
+                {/each}
+            {/await}
+        </div>
+    </div>
+    <br class="mt-4"/>
+    <div class="flex fixed bottom-0 right-0 m-2">
+        <div class="right-0">
+            <button class="btn btn-primary" on:click={()=> listCards = false}>Close</button>
         </div>
     </div>
 </Modal>
