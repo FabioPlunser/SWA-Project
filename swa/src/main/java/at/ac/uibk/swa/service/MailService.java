9823a0fd6abc3866dad6415fe59ea08f52a8e3e4
Luana Bach Gadelha <luana.bach-gadelha@student.uibk.ac.at>
2023-01-16 19:37:42 +0100

    added javadoc to MailService

--- swa/src/main/java/at/ac/uibk/swa/service/MailService.java
+++ swa/src/main/java/at/ac/uibk/swa/service/MailService.java
@@ -13,6 +13,10 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    /**
+     * Notifies all subscribers and the creator of a deck per email if the deck is blocked by admin.
+     * @param deck
+     */
     public void notifyBlockedDeck(Deck deck) {
         String[] recipients = deck.getSubscribedPersons().stream().map(Person::getEmail).toArray(String[]::new);
         sendMessage(String.format("Dear User, %nDeck \"%s\" does not comply with our policies and has been blocked!",
@@ -21,6 +25,10 @@ public class MailService {
                 recipients);
     }
 
+    /**
+     * Notifies all subscribers and the creator of a deck per email if the deck is unblocked by admin.
+     * @param deck
+     */
     public void notifyUnblockedDeck(Deck deck) {
         String[] recipients = deck.getSubscribedPersons().stream().map(Person::getEmail).toArray(String[]::new);
         sendMessage(String.format("Dear User, %nDeck \"%s\" complies with our policies and has been unblocked!",
@@ -29,6 +37,13 @@ public class MailService {
                 recipients);
     }
 
+    /**
+     * Sends an email to one or several people through the address "simpson-lisa@gmx.at".
+     * Recipients cannot see whether other people received the message too or who they would be
+     * @param text the content of the mail as String
+     * @param subject the subject of the mail as String
+     * @param recipients String array of email addresses of everyone who should receive the message
+     */
     public void sendMessage(String text, String subject, String[] recipients) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("simpson-lisa@gmx.at");
