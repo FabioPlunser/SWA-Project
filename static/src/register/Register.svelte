41b502e739b8f88da7fa6370c143d434a832d013
Frederik Hirsch <frederik.hirsch@student.uibk.ac.at>
2023-01-17 23:21:27 +0100

    changed register page to look similar to login page. Added "back" button

--- static/src/register/Register.svelte
+++ static/src/register/Register.svelte
@@ -53,9 +53,13 @@
 <SvelteToast/>
 <main class="flex justify-center items-center mx-auto h-screen text-white">
     <div class="rounded-xl shadow-2xl bg-slate-900 max-w-fit p-10">
-        <h1 class="font-bold text-2xl mx-auto flex justify-center p-2">Register</h1>
+        <div class="flex justify-center">
+            <img class="flex justify-center w-24" src={favicon} alt="favicon"/>
+            <h1 class="items-center text-3xl font-bold flex justify-center">Memori</h1>
+        </div>
+        <h1 class="font-bold text-2xl mx-auto flex justify-center pt-6 pb-2">Register</h1>
         <Form url="/api/register" method="POST" dataFormat={formFormat.FORM} {formValidators} on:postFetch={handlePostFetch}>
-            <div class="flex flex-col">
+            <div class="flex flex-col gap-2">
                 <div class="form-control">
                     <label class="input-group">
                     <span class="w-36">Username</span>
@@ -64,7 +68,6 @@
                     <FormError name="username" key="required" message="Username is required"/>
 
                 </div>
-                <br class="pt-4"/>
                 <div class="form-control">
                     <label class="input-group">
                     <span class="w-36">Email</span>
@@ -73,7 +76,6 @@
                     <FormError name="email" key="required" message="Email is required"/>
                     <FormError name="email" key="email" message="Email must be an official mail"/>
                 </div>
-                <br class="pt-4"/>
                 <div class="form-control">
                     <label class="input-group">
                     <span class="w-36">Password</span>
@@ -82,7 +84,8 @@
                     <FormError name="password" key="required" message="Password is required"/>
                     <FormError name="password" key="minLength" message="Password must be at least 8 characters"/>
                 </div>
-                <div class="flex justify-center mt-2">
+                <div class="flex justify-between">
+                    <button type="button" class="btn btn-secondary" on:click={()=>redirect("login")}>Back</button>
                     <button type="submit" class="btn btn-primary">Register</button>
                 </div>
             </div>
