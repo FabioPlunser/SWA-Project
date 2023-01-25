308fe3fc975d7962493143d946f35156ddf1745e
Fabian Margreiter <fabian.margreiter@gmail.com>
2023-01-25 21:09:28 +0100

    Fixed code smell critizing unnecessary import

--- swa/src/main/java/at/ac/uibk/swa/models/Person.java
+++ swa/src/main/java/at/ac/uibk/swa/models/Person.java
@@ -1,6 +1,5 @@
 package at.ac.uibk.swa.models;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
